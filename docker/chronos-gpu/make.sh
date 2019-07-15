1c1f5c4740dce23a13c696188c7f3c110bbb7e39
Sebastian <ga63fiy@mytum.de>
2019-07-15 15:48:02 +0200

    Added flag to docker compose file

--- docker/chronos-gpu/make.sh
+++ docker/chronos-gpu/make.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cp Dockerfile ../../.
 cd ../../.
-docker build --rm=true -t ga63fiy/chronos/0.0.1-gpu .
-docker push ga63fiy/chronos/0.0.1-gpu
+docker build --rm=true -t ga63fiy/chronos:0.0.1-gpu .
+docker push ga63fiy/chronos:0.0.1-gpu
 rm Dockerfile
