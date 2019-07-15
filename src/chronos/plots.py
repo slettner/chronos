0384b1ce3ac5b5fa04dd47f0926823b256614d85
Sebastian Lettner <laytneir@Sebastians-MacBook-Pro.local>
2019-07-15 19:30:02 +0200

    added plot routine

--- src/chronos/plots.py
+++ src/chronos/plots.py
@@ -120,3 +120,91 @@ def plot_prediction(
             plt.tight_layout()
             tf.logging.info("Saving prediction plot to: %s", save_plot + "/prediction_{}.pdf".format(s))
             fig.savefig(save_plot + "/prediction_{}.pdf".format(s))
+
+
+def plot_prediction_from_h5(
+        data,
+        prediction_data,
+        start_plot,
+        end_plot,
+        series,
+        save_plot=None,
+        plot_show=True
+):
+    """
+
+    Args:
+        data(AbstractInputGenerator):
+        prediction_data(h5py.DataSet):
+        start_plot(int): Start of the sequence to plot
+        end_plot(int): End of the sequence to plot
+        series(list): The series to plot
+        save_plot(str): Path for image
+        plot_show(bool): Show the plot
+
+    Returns:
+
+    """
+    train_predict = prediction_data["train"][:]
+    validation_predict = prediction_data["validation"][:]
+    test_predict = prediction_data["test"][:]
+    for s in series:
+        assert s < data.num_time_series - 1, "Data has only {} time series. Requested series {}".format(
+            data.num_time_series, s
+        )
+    assert len(train_predict.shape) == 2, "Expected Rank two for train_predict but got {}".format(
+        len(train_predict.shape)
+    )
+    assert len(validation_predict.shape) == 2, "Expected Rank two for validation_predict but got {}".format(
+        len(validation_predict.shape)
+    )
+    assert len(test_predict.shape) == 2, "Expected Rank two for test_predict but got {}".format(
+        len(test_predict.shape)
+    )
+    n = len(data)
+    #
+    # Create empty series of the same length of the data and set the values to nan
+    # This way, we can fill the appropriate section for train, valid, test so that
+    # when we print them, they appear at the appropriate loction with respect to the original timeseries
+    #
+    train_predict_plot = np.empty((n, 1))
+    train_predict_plot[:, :] = np.nan
+    validation_predict_plot = np.empty((n, 1))
+    validation_predict_plot[:, :] = np.nan
+    test_prediction_plot = np.empty((n, 1))
+    test_prediction_plot[:, :] = np.nan
+
+    #
+    # We use window data to predict a value at horizon from the end of the window, therefore start is
+    # is at the end of the horizon
+    #
+    for s in series:
+        if train_predict is not None:
+            train_predict_plot[data.train_range, 0] = train_predict[:, s]
+
+        if validation_predict is not None:
+            validation_predict_plot[data.validation_range, 0] = validation_predict[:, s]
+
+        if test_predict is not None:
+            test_prediction_plot[data.test_range, 0] = test_predict[:, s]
+
+        # Plotting the original series and whatever is available of trainPredictPlot, validPredictPlot and testPredictPlot
+        fig = plt.figure()
+
+        plt.plot(data[start_plot:end_plot, s], label="True Data", alpha=0.8)
+        plt.plot(train_predict_plot[start_plot:end_plot], label="Train Set Prediction")
+        plt.plot(validation_predict_plot[start_plot:end_plot], label="Val Set Prediction")
+        plt.plot(test_prediction_plot[start_plot:end_plot], label="Test Set Prediction")
+        plt.legend()
+        plt.ylabel("Timeseries")
+        plt.xlabel("Time")
+        plt.title("Prediction Plotting for timeseries # %d" % (s))
+
+        fig.canvas.set_window_title('Prediction')
+        if plot_show:
+            plt.show()
+
+        if save_plot is not None:
+            plt.tight_layout()
+            tf.logging.info("Saving prediction plot to: %s", save_plot + "/prediction_{}.pdf".format(s))
+            fig.savefig(save_plot + "/prediction_{}.pdf".format(s))
